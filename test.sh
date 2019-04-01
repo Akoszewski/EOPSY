bea50bc3d38afe02496cfb6a7b8dcc18a380a3c9
Akoszewski <est@onet.com.pl>
2019-04-01 20:23:43 +0200

    test

---
+++ test.sh
@@ -0,0 +1,5 @@
+./setup.sh
+output=""
+./modify -u "folder 1/file inside 1" "folder 1/folder inside/file inside deep 1"
+
+./modify -l "folder 1/FILE INSIDE 1" "folder 1/folder inside/FILE INSIDE DEEP 1"
